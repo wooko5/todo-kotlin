e757f9e803c191114115ecd03aa14673070dc84c
캐롯손해보험 <carrot@Neo-MacBook-Pro.local>
2024-07-04 15:28:11 +0900

    init toy project

---
+++ src/test/kotlin/carrotins/toy/ToyApplicationTests.kt
@@ -0,0 +1,13 @@
+package carrotins.toy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
